2f971f502669988bc9dc843a72d468dcb4554aee
Jace <jace.rhea@gmail.com>
2022-11-02 20:29:50 -0500

    Initial state.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngrx-presentation';
+  // count$: Observable<number>;
+
+  constructor() {
+    // TODO: Connect `this.count$` stream to the current store `count` state
+  }
+
+  increment() {
+    // TODO: Dispatch an increment action
+  }
+
+  decrement() {
+    // TODO: Dispatch a decrement action
+  }
+
+  reset() {
+    // TODO: Dispatch a reset action
+  }
+
+
 }
